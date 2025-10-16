6fdf8678b96f7814148bd77abd01117bd4b7e2e2
martinsram3k <79270832+martinsram3k@users.noreply.github.com>
2025-10-16 09:50:48 +0200

    Add new 'cas' project with initial implementation
    Introduced a new 'cas' project including Code::Blocks project file, initial main.cpp source file, and build artifacts. The main.cpp calculates hours and remaining minutes from a total number of seconds. Also updated the layout file for 'soucinpodilmacmin' to reflect cursor position changes.

---
+++ PRG/cas/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int s=620;
+
+
+    int h=s/60; // 10
+
+    int z=s-(h*60);
+
+
+
+    cout << s << " " << h << " " << z << endl;
+
+
+    return 0;
+}
