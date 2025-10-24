a4343ff391e0b7093dfc90ce39822cd5774b0761
martinsram3k <79270832+martinsram3k@users.noreply.github.com>
2025-10-24 09:39:58 +0200

    Refactor variable types and add layout file
    Changed variables a, b, c to be declared in a single line and updated 's' from double to int in main.cpp for clarity. Added trouhelnik.layout for CodeBlocks IDE project layout. Minor output formatting adjustments.

--- PRG/trouhelnik/main.cpp
+++ PRG/trouhelnik/main.cpp
@@ -3,10 +3,8 @@ using namespace std;
 
 int main() {
 
-   double a;
-   double b;
-   double c;
-   double s = 1;
+   double a, b, c;
+   int s = 1;
 
 
    cout << "Vlož èíslo A: ";
@@ -38,11 +36,11 @@ int main() {
    }
 
    if (s == 0) {
-    cout << "trouhelník nejde" << s << endl;
+    cout << "trouhelník nejde " << s << endl;
    }
 
    if (s == 1) {
-    cout << "trouhelník jde" << s << endl;
+    cout << "trouhelník jde " << s << endl;
    }
 
 }
