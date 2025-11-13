7b12028c92d93a2e28ef91b175f8dd7184def921
martinsram3k <79270832+martinsram3k@users.noreply.github.com>
2025-11-13 10:04:13 +0100

    Add initial urok project files
    Added main.cpp with interest calculation logic, Code::Blocks project file, and initial build outputs for the urok project.

---
+++ PRG/urok/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int vklad = 9999;
+    int vkladhold = 0;
+    double urok = 1.33;
+    int pocetlet = 10;
+
+    cout << "pocastecni vklad: " << vklad << endl;
+
+   for (int i = 0; i < (pocetlet + 1 ); i++) {
+        cout << i << ". rok :" << endl;
+if (i == 0) {
+   vkladhold = vklad * urok;
+} else {
+vkladhold = vkladhold * urok;
+}
+
+    cout << vkladhold << endl;
+  }
+
+    return 0;
+}
