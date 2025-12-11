402f903aa85a07819e61d6e4c5856ee67c8a819c
martinsram3k <79270832+martinsram3k@users.noreply.github.com>
2025-12-11 09:28:27 +0100

    Implement digit sum and product calculation
    Replaced the previous hello world code with logic to compute the sum and product of digits for a user-input natural number. The program now prompts for input, processes each digit, and outputs the results.

--- PRG/TEST.11.12.2025/main.cpp
+++ PRG/TEST.11.12.2025/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 
-using namespace std;
+int main() {
+    int cislo_vstup;
+    int cislo_k_praci;
+    int ciferny_soucin = 1;
+    int ciferny_soucet = 0;
+    int cifra;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+    std::cout << "Zadejte prirozene cislo : ";
+    std::cin >> cislo_vstup;
+
+    cislo_k_praci = cislo_vstup;
+
+    while (cislo_k_praci > 0) {
+        cifra = cislo_k_praci % 10;
+        ciferny_soucet += cifra;
+
+        ciferny_soucin *= cifra;
+        cislo_k_praci /= 10;
+    }
+
+    std::cout << "Ciferny soucet = " << ciferny_soucet << std::endl;
+    std::cout << "Ciferny soucin = " << ciferny_soucin << std::endl;
+
     return 0;
 }
