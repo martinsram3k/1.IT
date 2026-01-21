739b2a4994f354766528ce0c25a5fc4a8ad7055d
martinsram3k <79270832+martinsram3k@users.noreply.github.com>
2026-01-21 11:08:36 +0100

    Add automaty_vylepseno project and pozicovani web files
    Added new Code::Blocks project 'automaty_vylepseno' with initial main.cpp. Introduced new HTML and CSS files for '21-01-26-pozicovani' web example. Updated layout files for kostka, maty, and pozice_text projects to reflect recent editor state changes.

---
+++ PRG/automaty_vylepseno/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+   int slot1 = 0;
+   int slot2 = 0;
+   int slot3 = 0;
+   int penize = 0;
+   int sazka = 0;
+
+
+
+
+
+
+    return 0;
+}
