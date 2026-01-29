150de5a4fd285d60f8cac8c793cf287ff02cc2c0
martinsram3k <79270832+martinsram3k@users.noreply.github.com>
2026-01-29 10:05:05 +0100

    dfs

---
+++ PRG/matematicky_test/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+
+    const int pocetOtazek = 10;
+    const int maxCislo = 100;
+
+    cout << "------------------------------------" << endl;
+    cout << "| +-*/ |  MATEMATICKY TEST  | +-*/ |" << endl;
+    cout << "------------------------------------" << endl;
+
+
+    for (int i = 0; i < pocetOtazek; i++) {
+
+
+
+    cout << "      " << (i + 1) << ".  priklad:" << endl;
+
+
+    }
+
+    return 0;
+}
