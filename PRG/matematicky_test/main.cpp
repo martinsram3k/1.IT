eeff9a50d7f54a04c7c3109b6843370ff281cb0e
martinsram3k <79270832+martinsram3k@users.noreply.github.com>
2026-03-04 11:32:59 +0100

    snad

--- PRG/matematicky_test/main.cpp
+++ PRG/matematicky_test/main.cpp
@@ -8,8 +8,8 @@ int main()
 {
     srand(time(0));
 
-    const int pocetOtazek = 3;
-    const int maxCislo = 5;
+    const int pocetOtazek = 10;
+    const int maxCislo = 2;
 
     char operace;
     int vysledek;
@@ -25,8 +25,6 @@ int main()
         int cislo1, cislo2, znamenko;
         bool nalezeno = false;
 
-        // 1. LOGIKA: Nejprve najdeme validni cisla a spravny vysledek
-        // Cyklus bezi, dokud nevygeneruje priklad, ktery vyhovuje tvym pravidlum
         while (!nalezeno)
         {
             cislo1 = rand() % maxCislo;
@@ -37,40 +35,40 @@ int main()
             { // Plus
                 operace = '+';
                 vysledek = cislo1 + cislo2;
-                if (vysledek <= maxCislo && vysledek > 0 && cislo1 > 0 && cislo2 > 0) // Zajistime, ze vysledek bude v povolenem rozsahu a cisla budou nezaporna
+                if (vysledek <= maxCislo && vysledek > 0 && cislo1 > 0 && cislo2 > 0) 
                     nalezeno = true;
                     else if (vysledek == 0 || vysledek == 1)
                         {
-                            nalezeno = false; // Zamezime scitani nulou nebo jednickou, ktere by mohlo zpusobit zmatek
+                            nalezeno = false; 
                         }
             }
             else if (znamenko == 2)
             { // Minus
                 operace = '-';
                 vysledek = cislo1 - cislo2;
-                if (vysledek > 0 && vysledek <= maxCislo && cislo1 >= cislo2 && cislo1 > 0 && cislo2 > 0) // Zajistime, ze nevznikne zaporne cislo a vysledek bude v povolenem rozsahu
+                if (vysledek > 0 && vysledek <= maxCislo && cislo1 >= cislo2 && cislo1 > 0 && cislo2 > 0) 
                     nalezeno = true;
                     else if (vysledek == 0 || vysledek == 1)
                     {
-                        nalezeno = false; // Zamezime odecteni nulou nebo jednickou, ktere by mohlo zpusobit zmatek
+                        nalezeno = false; 
                     }
-                    
+
             }
             else if (znamenko == 3)
             { // Krat
                 operace = '*';
                 vysledek = cislo1 * cislo2;
-                if (vysledek <= maxCislo && cislo1 != 0 && cislo2 != 0 && vysledek > 0 && cislo1 > 1 && cislo2 > 1) // Zajistime, ze vysledek bude v povolenem rozsahu a cisla budou nezaporna a nenulova
+                if (vysledek <= maxCislo && cislo1 != 0 && cislo2 != 0 && vysledek > 0 && cislo1 > 1 && cislo2 > 1) 
                     nalezeno = true;
                     else if (vysledek == 0 || vysledek == 1)
                     {
-                        nalezeno = false; // Zamezime vynasobeni nulou, ktere by mohlo zpusobit zmatek
+                        nalezeno = false; 
                     }
             }
             else if (znamenko == 4)
             { // Deleno
                 operace = '/';
-                // Nesmime delit nulou a chceme cele cislo (zbytek po deleni je 0)
+             
                 if (cislo2 != 0 && cislo1 % cislo2 == 0 && cislo1 > 0 && cislo2 > 0 && vysledek > 0)
                 {
                     vysledek = cislo1 / cislo2;
@@ -78,44 +76,44 @@ int main()
                         nalezeno = true;
                 } else if (vysledek == 0 || vysledek == 1)
                 {
-                    nalezeno = false; // Zamezime deleni nulou a deleni jednickou, ktere by mohlo zpusobit zmatek
+                    nalezeno = false; 
                 }
-                
+
             }
         }
 
-        // 2. VYPIS: Ted uz mame spravna cisla i spravny vysledek v pameti
+       
         int cisloPrikladu = i + 1;
 
         if (cisloPrikladu < 10)
         {
-            // Odsazeni pro jednociferna cisla (1-9)
+            
             cout << "       " << cisloPrikladu << ".  priklad: ";
         }
         else if (cisloPrikladu < 100)
         {
-            // Odsazeni pro dvouciferna cisla (10-99) - o jednu mezeru mene
+           
             cout << "      " << cisloPrikladu << ".  priklad: ";
         }
         else if (cisloPrikladu < 1000)
         {
-            // Odsazeni pro trojciferna cisla (100-999) - o jednu mezeru mene
+           
             cout << "     " << cisloPrikladu << ".  priklad: ";
         }
         else
         {
-            // Odsazeni pro ctyrciferna cisla (1000+)
+            
             cout << "   " << cisloPrikladu << ".  priklad: ";
         }
 
-        // Samotne zobrazeni prikladu a nacteni odpovedi
+       
         cout << cislo1 << " " << operace << " " << cislo2 << " = ";
         cin >> odpoved;
 
-        // 3. KONTROLA
+       
         if (odpoved == vysledek)
         {
-            // Zde muzes neco vypsat, nebo nechat prazdne
+           
         }
         else
         {
