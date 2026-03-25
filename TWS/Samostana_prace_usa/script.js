e61bfc247ab9eca0ff562f2c08c5ae11cb0ae133
martinsram3k <79270832+martinsram3k@users.noreply.github.com>
2026-03-25 10:02:30 +0100

    Add USA gallery page and assets
    Introduce a new gallery for Samostana_prace_usa: add TWS/Samostana_prace_usa/gallery.html, script.js, img/icon and img/icon.png. Update TWS/Samostana_prace_usa/index.html and style.css to integrate the new page and styling. .DS_Store metadata files were also updated.

---
+++ TWS/Samostana_prace_usa/script.js
@@ -0,0 +1,21 @@
+// Definujeme dvě zprávy, které se budou střídat
+const zprava1 = "Vítejte v USA 🇺🇸";
+const zprava2 = "Jsme nejelpší! 🌟";
+
+
+let ukazujiPrvni = true;
+
+
+setInterval(() => {
+    if (ukazujiPrvni) {
+   
+        document.title = zprava2;
+    } else {
+       
+        document.title = zprava1;
+    }
+    
+    
+    ukazujiPrvni = !ukazujiPrvni;
+
+}, 1000); 
