8ad23f6f3e3573cb3fbb55fcf4f1bf12b76af45d
martinsram3k <79270832+martinsram3k@users.noreply.github.com>
2026-04-24 09:50:07 +0200

    Add podprogramy project with interest calculator
    Add a new Code::Blocks project 'podprogramy'. Introduces main.cpp implementing a simple yearly interest calculator (castkaPoRoce) that prompts for initial amount, number of years and per-year interest rates and prints the amount after each year. Includes project files (podprogramy.cbp, podprogramy.depend) and build artifacts (bin/Debug/podprogramy.exe, obj/Debug/main.o).

---
+++ PRG/podprogramy/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+
+double castkaPoRoce(double aktualniCastka, double urok)
+{
+    return aktualniCastka + (aktualniCastka / 100 * urok);
+}
+
+
+int main()
+{
+    double vCastka; 
+    int pocetLet;
+
+    cout << "Zadej vstupni castku: " << endl;
+    cin >> vCastka;
+    cout << "Zadej pocet let sporeni: " << endl;
+    cin >> pocetLet;
+
+    for (int i = 0; i < pocetLet; i++)
+    {
+       double urok;
+       cout << "Zadej urok % v " << i + 1 << ". roce : " << endl;
+       cin >> urok;
+
+       vCastka = castkaPoRoce(vCastka, urok); 
+       
+       cout << "Castka po " << i + 1 << ". roce: " << vCastka << endl;
+    }
+
+    return 0;
+}
