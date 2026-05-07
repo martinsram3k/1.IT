e11921133dd6b3f39fea841d3b48bd3d92ecbcb7
martinsram3k <79270832+martinsram3k@users.noreply.github.com>
2026-05-07 09:53:28 +0200

    .

--- PRG/razeniOdporu/main.cpp
+++ PRG/razeniOdporu/main.cpp
@@ -4,10 +4,8 @@
 using namespace std;
 
 int main() {
-    double r1, r2;
-
-    cin >> r1;
-    cin >> r2;
+    double r1 = 50;
+    double r2 = 100;
 
     cout << "Seriove zapojeni rezistoru " << r1 << " ohmu a " << r2 << " ohmu je " << seriove(r1, r2) << " ohmu." << endl;
     cout << "Paralelni zapojeni rezistoru " << r1 << " ohmu a " << r2 << " ohmu je " << paralelni(r1, r2) << " ohmu." << endl;
