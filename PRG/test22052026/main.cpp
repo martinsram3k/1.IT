38fc16ed8c04d56205acbfbb606e7992e9c70b73
martinsram3k <79270832+martinsram3k@users.noreply.github.com>
2026-05-22 10:36:53 +0200

    Update main.cpp

--- PRG/test22052026/main.cpp
+++ PRG/test22052026/main.cpp
@@ -1,12 +1,51 @@
 #include <iostream>
-#include "vypocty.h"
 
 using namespace std;
+)
+bool cifryJsouVzestupne(int cislo) {
 
-int main()
-{
+    if (cislo < 10) return true;
+
+    int posledniCifra = cislo % 10;
+    cislo /= 10;
+
+    while (cislo > 0) {
+        int aktualniCifra = cislo % 10;
+
+        if (aktualniCifra + 1 != posledniCifra) {
+            return false;
+        }
+
+        posledniCifra = aktualniCifra;
+        cislo /= 10;
+    }
+
+    return true;
+}
+
+int main() {
+    int cislo;
+    int poradoveCislo = 1;
+    int pocetSplnenych = 0;
 
 
+    while (true) {
+        cout << "Vlozte " << poradoveCislo << ". cislo: ";
+        cin >> cislo;
+
+
+        if (cislo == 0) {
+            break;
+        }
+
+        if (cifryJsouVzestupne(cislo)) {
+            pocetSplnenych++;
+        }
+
+        poradoveCislo++;
+    }
+
+    cout << "Pocet cisel se vzestupnymi ciframi: " << pocetSplnenych << endl;
 
     return 0;
 }
